7955eed275eca945c964083bfb0745869946e5fb
vinod patil <vinodptl02@gmail.com>
2020-10-13 17:24:54 +0100

    BikeModelcontroller full working and table button partialview

---
+++ BikeManegement/Controllers/BikeModelController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BikeManegement.Data;
+using BikeManegement.Models;
+using BikeManegement.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BikeManegement.Controllers
+{
+    public class BikeModelController : Controller
+    {
+
+        private readonly ApplicationDBContext _dbcontext;
+
+
+        [BindProperty]
+        public Bike_ModelViewModel BMViewmodel { get; set; }
+
+        public BikeModelController(ApplicationDBContext dBContext)
+        {
+            _dbcontext = dBContext;
+            BMViewmodel = new Bike_ModelViewModel()
+            {
+
+                BikeModel = new Models.BikeModel(),
+                BikeMakers = _dbcontext.BikeMakers.ToList()
+
+            };
+        }
+
+
+        public IActionResult Index()
+        {
+            var data = _dbcontext.bikeModels.Include(m => m.BikeMaker);
+            return View(data.ToList());
+
+        }
+
+
+        public IActionResult Create()
+        {
+            return View(BMViewmodel);
+        }
+
+
+
+
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateModel()
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(BMViewmodel);
+            }
+          
+            _dbcontext.bikeModels.Add(BMViewmodel.BikeModel);
+            _dbcontext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        //HTTP Get Method
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+
+
+
+            BMViewmodel.BikeModel = _dbcontext.bikeModels.Include(m => m.BikeMaker).SingleOrDefault(m => m.Id == id);
+
+            if (BMViewmodel.BikeModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(BMViewmodel);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+
+            BikeModel data = _dbcontext.bikeModels.Find(id);
+            
+
+            if(data == null)
+            {
+                return NotFound();
+            }
+            _dbcontext.bikeModels.Remove(data);
+            _dbcontext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditModel()
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(BMViewmodel);
+            }
+
+
+            _dbcontext.Update(BMViewmodel.BikeModel);
+            _dbcontext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+
+
+        }
+
+        
+
+
+
+
+    }
+
+}
---
+++ BikeManegement/ViewModels/Bike_ModelViewModel.cs
@@ -0,0 +1,40 @@
+﻿using BikeManegement.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeManegement.ViewModels
+{
+    public class Bike_ModelViewModel
+    {
+
+        public BikeModel BikeModel { get; set; }
+
+        public IEnumerable<BikeMaker> BikeMakers { get; set; }
+
+        public IEnumerable<SelectListItem> CBikeMakers(IEnumerable<BikeMaker> items)
+        {
+            List<SelectListItem> MakeList = new List<SelectListItem>();
+
+            SelectListItem sli = new SelectListItem
+            {
+                Text = "---select----",
+                Value = "0",
+            };
+            MakeList.Add(sli);
+            foreach(BikeMaker bm in items)
+            {
+                sli = new SelectListItem
+                {
+                    Text = bm.MakerName,
+                    Value = bm.Id.ToString()
+                };
+                MakeList.Add(sli);
+            }
+            return MakeList;
+
+        }
+    }
+}
