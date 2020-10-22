7fc70cf458f8144a299833b1623a5e378fde689e
vinod patil <vinodptl02@gmail.com>
2020-10-22 15:26:57 +0100

    Addition of Extension Method

---
+++ BikeManegement/Extensions/DropDownExtension.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeManegement.Extensions
+{
+    public static class DropDownExtension
+    {
+
+        public  static IEnumerable<SelectListItem> ToselectListItems<T>(this IEnumerable<T> items)
+        {
+            List<SelectListItem> DropdownList = new List<SelectListItem>();
+
+            SelectListItem sli = new SelectListItem
+            {
+                Text = "---select----",
+                Value = "0",
+            };
+            DropdownList.Add(sli);
+            foreach (var Item in items)
+            {
+                sli = new SelectListItem
+                {
+                    //Text = Item.GetType().GetProperty("Name").GetValue(Item, null).ToString(),
+                    //Value = Item.GetType().GetProperty("Id").GetValue(Item, null).ToString(),
+
+                    Text = Item.GetPropertyValue("Name"),
+                    Value = Item.GetPropertyValue("Id")
+
+                };
+                DropdownList.Add(sli);
+            }
+            return DropdownList;
+
+        }
+    }
+}
---
+++ BikeManegement/Extensions/ReflectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeManegement.Extensions
+{
+    public static class ReflectionExtension
+    {
+
+        public static string GetPropertyValue<T>(this T Item, string ProperyName)
+        {
+            return Item.GetType().GetProperty(ProperyName).GetValue(Item, null).ToString();
+        }
+    }
+}
